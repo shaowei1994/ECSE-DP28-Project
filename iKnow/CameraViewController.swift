56dc19bbe78b4c5d9dc068625a8b177331c938b3
Will <shao-wei.liang@mail.mcgill.ca>
2018-01-29 03:33:19 -0500

    added two child view

---
+++ iKnow/CameraViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  iKnow
+//
+//  Created by Shao-Wei Liang on 2018-01-17.
+//  Copyright © 2018 Shao-Wei Liang. All rights reserved.
+//
+import UIKit
+import AVKit
+import Vision
+import ARKit
+
+class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    @IBOutlet weak var cameraView: SCNView!
+    
+    private var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //instantiate a Capture Session
+        let captureSession = AVCaptureSession()
+        
+        //instantiate a Capturing Device
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {return}
+        
+        //enable auto-focus mode
+        if captureDevice.isFocusModeSupported(.continuousAutoFocus){
+            try! captureDevice.lockForConfiguration()
+            captureDevice.focusMode = .continuousAutoFocus
+            captureDevice.unlockForConfiguration()
+        }
+        
+        //instantiate the camera as a capture input for the capture session
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        //Set up the display layer on screen
+//        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+//        self.view.layer.a(previewLayer, at: 0)
+//        previewLayer.frame = self.view.frame
+//        previewLayer.frame = cameraView.bounds
+//        self.view.backgroundColor = .black
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        cameraView.layer.addSublayer(previewLayer)
+        previewLayer!.frame = cameraView.bounds
+        
+        //instantiate an "output" to be fed into capture session
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "Queue"))
+        captureSession.addOutput(dataOutput)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {return}
+        
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
+            guard let firstObservation = results.first else {return}
+            let confidence = String(format: "%.2f", firstObservation.confidence*100)
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            DispatchQueue.main.async {
+//                self.detailLabel.text = String(firstObservation.identifier.split(separator: ",")[0]) + " " + confidence + "%"
+            }
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+  
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ iKnow/MasterViewController.swift
@@ -0,0 +1,112 @@
+//
+//  MasterViewController.swift
+//  iKnow
+//
+//  Created by Shao-Wei Liang on 2018-01-28.
+//  Copyright © 2018 Shao-Wei Liang. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class MasterViewController: UIViewController {
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupView() {
+        setupSegmentedControl()
+        
+        updateView()
+    }
+    
+    private func setupSegmentedControl() {
+        // Configure Segmented Control
+        segmentedControl.removeAllSegments()
+        segmentedControl.insertSegment(withTitle: "Camera", at: 0, animated: false)
+        segmentedControl.insertSegment(withTitle: "Photo", at: 1, animated: false)
+        segmentedControl.addTarget(self, action: #selector(selectionDidChange(_:)), for: .valueChanged)
+        
+        // Select First Segment
+        segmentedControl.selectedSegmentIndex = 0
+    }
+    
+    @objc func selectionDidChange(_ sender: UISegmentedControl) {
+        updateView()
+    }
+    
+    private lazy var cameraViewController: CameraViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "CameraViewController") as! CameraViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    private lazy var photoViewController: PhotoViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "PhotoViewController") as! PhotoViewController
+        
+        // Add View Controller as Child View Controller
+        self.add(asChildViewController: viewController)
+        
+        return viewController
+    }()
+    
+    private func add(asChildViewController viewController: UIViewController) {
+        // Add Child View Controller
+        addChildViewController(viewController)
+        
+        // Add Child View as Subview
+        view.addSubview(viewController.view)
+        
+        // Configure Child View
+        viewController.view.frame = view.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        // Notify Child View Controller
+        viewController.didMove(toParentViewController: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        // Notify Child View Controller
+        viewController.willMove(toParentViewController: nil)
+        
+        // Remove Child View From Superview
+        viewController.view.removeFromSuperview()
+        
+        // Notify Child View Controller
+        viewController.removeFromParentViewController()
+    }
+    
+    private func updateView() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            remove(asChildViewController: photoViewController)
+            add(asChildViewController: cameraViewController)
+        } else {
+            remove(asChildViewController: cameraViewController)
+            add(asChildViewController: photoViewController)
+        }
+    }
+    
+}
