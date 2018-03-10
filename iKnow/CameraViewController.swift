963f807fdad8a4bae3df08615953490764696dfd
Will <shao-wei.liang@mail.mcgill.ca>
2018-03-09 19:12:57 -0500

    [System] Changed the flow of recognition process

--- iKnow/CameraViewController.swift
+++ iKnow/CameraViewController.swift
@@ -15,8 +15,9 @@ class CameraViewController: UIViewController, ARSKViewDelegate, ARSessionDelegat
     @IBOutlet weak var detailLabel: UILabel!
     @IBOutlet weak var cameraView: ARSKView!
     private let visionQueue = DispatchQueue(label: "Queue") // A Serial Queue
-    private var suspended = false
-    private let orientation = CGImagePropertyOrientation(UIDevice.current.orientation)
+//    private var suspended = false
+//    private let orientation = CGImagePropertyOrientation(UIDevice.current.orientation)
+    private var currentBuffer: CVPixelBuffer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +32,7 @@ class CameraViewController: UIViewController, ARSKViewDelegate, ARSessionDelegat
         //Set the scene to the view
         cameraView.presentScene(scene)
         cameraView.session.delegate = self
-        loopProcess()
+//        loopProcess()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,45 +47,109 @@ class CameraViewController: UIViewController, ARSKViewDelegate, ARSessionDelegat
             cameraView.session.run(configuration)
         }
         
-        if suspended == true{
-            visionQueue.resume()
-            suspended = false
-        }
+//        if suspended == true{
+//        visionQueue.resume()
+//            suspended = false
+//        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         cameraView.session.pause()
-        visionQueue.suspend()
-        suspended = true
+//        visionQueue.suspend()
+//        suspended = true
     }
     
-    func loopProcess() {
-        visionQueue.async {
-            self.objectRecognition()
-            self.loopProcess()
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        // Do not enqueue other buffers for processing while another Vision task is still running.
+        guard currentBuffer == nil, case .normal = frame.camera.trackingState else { return }
+        // Retain the image buffer for Vision processing.
+        self.currentBuffer = frame.capturedImage
+        classifyCurrentFrame()
+    }
+    
+    // Vision classification request and model
+    private lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            // Instantiate the model from its generated Swift class.
+            let model = try VNCoreMLModel(for: Inceptionv3().model)
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            
+            // Crop input images to square area at center, matching the way the ML model was trained.
+            request.imageCropAndScaleOption = .centerCrop
+            
+            // Use CPU for Vision processing to ensure that there are adequate GPU resources for rendering.
+            request.usesCPUOnly = true
+            
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
         }
-    }
+    }()
     
-    private func objectRecognition(){
-        guard let pixelBuffer: CVPixelBuffer = cameraView.session.currentFrame?.capturedImage else {return}
-        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
-        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {return}
-        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
-            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
-            guard let firstObservation = results.first else {return}
-            DispatchQueue.main.async {
-                self.detailLabel.text = String(firstObservation.identifier.split(separator: ",")[0])
-                print(firstObservation.identifier.split(separator: ",")[0], firstObservation.confidence)
+    // Run the Vision+ML classifier on the current image buffer.
+    private func classifyCurrentFrame() {
+        let orientation = CGImagePropertyOrientation(UIDevice.current.orientation)
+        let requestHandler = VNImageRequestHandler(cvPixelBuffer: currentBuffer!, orientation: orientation)
+        visionQueue.async {
+            do {
+                // Release the pixel buffer when done, allowing the next buffer to be processed.
+                defer { self.currentBuffer = nil }
+                try requestHandler.perform([self.classificationRequest])
+            } catch {
+                print("Error: Vision request failed with error \"\(error)\"")
             }
         }
-        // Crop input images to square area at center, matching the way the ML model was trained.
-        request.imageCropAndScaleOption = .centerCrop
-
-        // Use CPU for Vision processing to ensure that there are adequate GPU resources for rendering.
-        request.usesCPUOnly = true
-        try? VNImageRequestHandler(ciImage: ciImage, orientation: orientation).perform([request])
     }
+    
+    // Handle completion of the Vision request and choose results to display.
+    func processClassifications(for request: VNRequest, error: Error?) {
+        guard let results = request.results else {
+            print("Unable to classify image.\n\(error!.localizedDescription)")
+            return
+        }
+        // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+        let classifications = results as! [VNClassificationObservation]
+        
+        // Show a label for the highest-confidence result (but only above a minimum confidence threshold).
+        guard let bestResult = classifications.first else { return }
+        guard let label = bestResult.identifier.split(separator: ",").first else { return }
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.detailLabel.text = String(label)
+            print(label, bestResult.confidence)
+        }
+    }
+    
+ 
+//    func loopProcess() {
+//        visionQueue.async {
+//            self.objectRecognition()
+//            self.loopProcess()
+//        }
+//    }
+//
+//    private func objectRecognition(){
+//        guard let pixelBuffer: CVPixelBuffer = cameraView.session.currentFrame?.capturedImage else {return}
+//        let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+//        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {return}
+//        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+//            guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
+//            guard let firstObservation = results.first else {return}
+//            DispatchQueue.main.async {
+//                self.detailLabel.text = String(firstObservation.identifier.split(separator: ",")[0])
+//                print(firstObservation.identifier.split(separator: ",")[0], firstObservation.confidence)
+//            }
+//        }
+//        // Crop input images to square area at center, matching the way the ML model was trained.
+//        request.imageCropAndScaleOption = .centerCrop
+//
+//        // Use CPU for Vision processing to ensure that there are adequate GPU resources for rendering.
+//        request.usesCPUOnly = true
+//        try? VNImageRequestHandler(ciImage: ciImage, orientation: orientation).perform([request])
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
