88b4ea1d432e0e38af79f631082c110f234c599d
Will <shao-wei.liang@mail.mcgill.ca>
2018-03-27 23:27:56 -0400

    [Settings] Reordering the language list now saved permanently within the device.

--- iKnow/ViewControllers/SettingsViewController.swift
+++ iKnow/ViewControllers/SettingsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SettingsViewController: UITableViewController {
-
+    
     let cellHeight: CGFloat = 50
     let fontSize: CGFloat = 35
     
@@ -20,6 +20,11 @@ class SettingsViewController: UITableViewController {
         "About"
     ]
     
+    let abouts: [String]  = [
+        "Version",
+        "Build"
+    ]
+    
     var languages: [Language] = [
         Language(symbol: "🇨🇦", name: "Canadian English"),
         Language(symbol: "🇨🇳", name: "Simplified Chinese"),
@@ -27,21 +32,6 @@ class SettingsViewController: UITableViewController {
         Language(symbol: "🇯🇵", name: "Japanese"),
         Language(symbol: "🇫🇷", name: "French")
     ]
-    
-    let abouts: [String]  = [
-        "Version",
-        "Build"
-    ]
-
-    class Language{
-        var symbol: String
-        var name: String
-        
-        init(symbol: String, name: String){
-            self.symbol = symbol
-            self.name = name
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,10 +43,12 @@ class SettingsViewController: UITableViewController {
         let tableViewEditingMode = tableView.isEditing
         tableView.setEditing(!tableViewEditingMode, animated: true)
     }
-
+    
     // MARK: - Table view data source
-
+    
     @IBAction func didSelectDoneButton(_ sender: Any) {
+        let encodedData = NSKeyedArchiver.archivedData(withRootObject: languages)
+        UserDefaults.standard.set(encodedData, forKey: "LanguageSortOrder")
         self.dismiss(animated: true, completion: nil)
     }
     
@@ -64,7 +56,7 @@ class SettingsViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 2
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         if section == 0 {
@@ -73,7 +65,7 @@ class SettingsViewController: UITableViewController {
             return abouts.count
         }
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "LanguageCell", for: indexPath)
         switch (indexPath.section) {
@@ -97,7 +89,7 @@ class SettingsViewController: UITableViewController {
         let movedLanguageCell = languages.remove(at: fromIndexPath.row)
         languages.insert(movedLanguageCell, at: to.row)
     }
-
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
@@ -118,7 +110,7 @@ class SettingsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sections[section]
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
             cell.accessoryType = .checkmark
@@ -135,7 +127,14 @@ class SettingsViewController: UITableViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-       
+        if let data = UserDefaults.standard.data(forKey: "LanguageSortOrder"),
+            let languages = NSKeyedUnarchiver.unarchiveObject(with: data) as? [Language] {
+            self.languages = languages
+        }
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+    }
+    
 }
