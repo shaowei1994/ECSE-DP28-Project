b787d71ef6e437a29353cf70086a101e7ad65182
Will <shao-wei.liang@mail.mcgill.ca>
2018-03-28 21:12:50 -0400

    [Localization] Added language files

---
+++ iKnow/Localizations/languageList.swift
@@ -0,0 +1,22 @@
+//
+//  languageList.swift
+//  iKnow
+//
+//  Created by Shao-Wei Liang on 2018-03-28.
+//  Copyright © 2018 Shao-Wei Liang. All rights reserved.
+//
+
+import Foundation
+
+var languageList : [String : [String : String]] = [
+    "🇨🇳 - Simplified Chinese"  : simpChinese,
+    "🇹🇼 - Traditional Chinese" : tradChinese,
+    "🇯🇵 - Japanese"            : japanese,
+    "🇫🇷 - French"              : french,
+    "🇩🇪 - German"              : german,
+    "🇮🇹 - Italian"             : italian,
+    "🇰🇷 - Korean"              : korean,
+    "🇷🇺 - Russian"             : russian,
+    "🇬🇷 - Greek"               : greek,
+    "🇪🇸 - Spanish"             : spanish
+]
